9d625e1e181fe5fbec3aba37905b16f284630863
Kelwin77-7 <kel5044rac@gmail.com>
2026-05-27 14:43:27 -0600

    modificacion en metodo tylor e index

--- mysitedjango/calculadora/views.py
+++ mysitedjango/calculadora/views.py
@@ -13,14 +13,19 @@ def home(request):
     if request.method == "POST":
         funcion = request.POST.get("funcion")
         punto_a_input = request.POST.get("punto_a", "0")
-        punto_a = float(punto_a_input) if punto_a_input else 0
+        n_terminos = int(request.POST.get("n_terminos", 5))
+        try:
+            punto_a = sp.sympify(punto_a_input) if punto_a_input else 0
 
-        # Llamamos a la lógica principal
-        resultado = calcular_taylor_maclaurin(funcion, punto_a)
+            # Llamamos a la lógica principal
+            resultado = calcular_taylor_maclaurin(funcion, punto_a, n_terminos)
 
-        # Si no hay errores, generamos la gráfica
-        if resultado and not resultado.get('error'):
-            serie_obj = resultado['resultado_final_raw']
-            resultado['grafica'] = generar_grafica(funcion, punto_a, serie_obj)
+            # Si no hay errores, generamos la gráfica
+            if resultado and not resultado.get('error'):
+                serie_obj = resultado['resultado_final_raw']
+                resultado['grafica'] = generar_grafica(
+                    funcion, punto_a, serie_obj)
+        except Exception as e:
+            resultado = {'error': str(e)}
 
     return render(request, "calculadora/index.html", {"resultado": resultado})
